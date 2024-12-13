44488346763816a397b25aaa34c74c5caf76eb29
VII073 <thanabordee073@gmail.com>
2024-12-13 16:30:06 +0700

    week3

---
+++ src/Traffic.cpp
@@ -0,0 +1,57 @@
+/*
+
+#include <Arduino.h>
+#define SW 15
+#define GR1 23
+#define Y1 19
+#define R1 18
+#define GR2 5
+#define Y2 17
+#define R2 16
+#define ALL_light (15,23,19,18,5,17,16) 
+
+
+
+void setup() {
+  pinMode(SW,INPUT);
+  pinMode(R1, OUTPUT);
+  pinMode(Y2, OUTPUT);
+  pinMode(GR1, OUTPUT);
+  
+  pinMode(R2, OUTPUT);
+  pinMode(Y2, OUTPUT);
+  pinMode(GR2, OUTPUT);
+
+}
+void loop () {
+ int digitalRead(SW);
+//เขียว
+digitalWrite (GR1, HIGH);
+digitalWrite (R1, LOW);
+digitalWrite (Y1, LOW);
+digitalWrite (GR2,LOW);
+digitalWrite (R1, HIGH);
+digitalWrite (Y2, LOW);
+delay(700);
+//เหลือง
+digitalWrite (GR1, LOW);
+digitalWrite (R1, LOW);
+digitalWrite (Y1, HIGH);
+digitalWrite (GR2, LOW);
+digitalWrite (R1, LOW);
+digitalWrite (Y2, HIGH);
+delay(700);
+//แดง
+digitalWrite (GR1, LOW);
+digitalWrite (R1, HIGH);
+digitalWrite (Y1, LOW);
+digitalWrite (GR2, HIGH);
+digitalWrite (R1, LOW);
+digitalWrite (Y2, LOW);  
+if (SW,HIGH);
+
+    
+
+}
+
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,70 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define  L1 23
+#define  L2 19
+#define  L3 18
+#define  L4 5
+#define  L5 17
+#define  L6 16
+#define  L7 4
+#define  L8 0
+#define Input 36
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+void setup(){
+  pinMode(L1,OUTPUT);
+  pinMode(L2,OUTPUT);
+  pinMode(L3,OUTPUT);
+  pinMode(L4,OUTPUT);
+  pinMode(L5,OUTPUT);
+  pinMode(L6,OUTPUT);
+  pinMode(L7,OUTPUT);
+  pinMode(L8,OUTPUT);
+  pinMode(Input,INPUT); 
+  }
+  
+void loop (){
+ int sensorValue = analogRead(Input);
+ 
+ if (sensorValue <= 500){
+  digitalWrite(L1,HIGH);
+  digitalWrite(L2,LOW);
+  digitalWrite(L3,LOW);
+  digitalWrite(L4,LOW);
+  digitalWrite(L5,LOW);
+  digitalWrite(L6,LOW);
+  digitalWrite(L7,LOW);
+  digitalWrite(L8,LOW);
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+ } else if(sensorValue <= 1000) {
+ digitalWrite(L1,HIGH);
+ digitalWrite(L2,HIGH);
+ digitalWrite(L3,HIGH);
+ digitalWrite(L4,LOW);
+ digitalWrite(L5,LOW);
+ digitalWrite(L6,LOW);
+ digitalWrite(L7,LOW);
+ digitalWrite(L8,LOW);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ } else if (sensorValue <= 2000){
+ digitalWrite(L1,HIGH);
+ digitalWrite(L2,HIGH);
+ digitalWrite(L3,HIGH);
+ digitalWrite(L4,HIGH);
+ digitalWrite(L5,HIGH);
+ digitalWrite(L6,LOW);
+ digitalWrite(L7,LOW);
+ digitalWrite(L8,LOW);
+ 
+  } else {
+ digitalWrite(L1,HIGH);
+ digitalWrite(L2,HIGH);
+ digitalWrite(L3,HIGH);
+ digitalWrite(L4,HIGH);
+ digitalWrite(L5,HIGH);
+ digitalWrite(L6,HIGH);
+ digitalWrite(L7,HIGH);
+ digitalWrite(L8,HIGH);
+  
+ }
+ 
 }
